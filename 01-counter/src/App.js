07822ece00a66396668608c529594025e18fb9ca
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-10-25 20:52:31 -0300

    Add actions and reducer

--- 01-counter/src/App.js
+++ 01-counter/src/App.js
@@ -1,13 +1,39 @@
 import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment, incrementByAmount, reset } from './redux/counter'
+
+export function App() {
+  
+  // useSelector for Reading data from store. state.<nombreReducerEnStore>.<valorDelState> 
+  const count = useSelector(state => state.counter.count)
+  
+  // useDispatch to send actions (imported "decrement / increment")
+  const dispatch = useDispatch()
+
+  //Local state for the amount to pass to incrementByAmount action
+  const [amount, setAmount] = useState(1)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        Redux Toolkit
-      </header>
+    <div className='App'>
+      <header> Redux Toolkit </header>
+      
+      
+      <button onClick={ () => dispatch(increment()) }> Increment </button>
+      
+      <span> {count} </span>
+      
+      <button onClick={ () => dispatch(decrement()) }> Decrement </button>
+
+      <div>
+        <button onClick={ () => dispatch(incrementByAmount(amount)) }> Increment by: </button>
+        <input type={"number"} value={amount} onChange={(e)=>setAmount(parseInt(e.target.value))} />
+      </div>
+      
+      <button onClick={ () => dispatch(reset()) }> Reset </button>
     </div>
-  );
+  )
 }
 
 export default App;
